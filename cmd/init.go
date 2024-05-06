cec850133bd924726ef4aa50519a43c4a836c202
Jaka Krajnc <jaka.krajnc@agiledrop.com>
2024-05-06 16:35:08 +0200

    final

--- cmd/init.go
+++ cmd/init.go
@@ -1,27 +1,20 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
-	"fmt"
-
+	"github.com/jaka-k/divrhino-studybudy/data"
 	"github.com/spf13/cobra"
 )
 
 // initCmd represents the init command
 var initCmd = &cobra.Command{
 	Use:   "init",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Initialise a new studybuddy db and table",
+	Long:  "Initialise a new studybuddy db and table",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("init called")
+		data.CreateTable()
 	},
 }
 
